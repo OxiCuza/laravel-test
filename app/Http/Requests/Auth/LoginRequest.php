34c336a0ab79c22ca7b573e4cb6a6b594fd77686
oxicuza <oxicusa@gmail.com>
2024-01-29 11:24:29 +0700

    feat(auth): create login register request

---
+++ app/Http/Requests/Auth/LoginRequest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Traits\FailValidationTrait;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Validation\Rules\Password;
+
+class LoginRequest extends FormRequest
+{
+    use FailValidationTrait;
+
+    /**
+     * Indicates whether validation should stop after the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => ['required', 'string', 'email', 'max:255'],
+            'password' => ['required', Password::min(8)],
+        ];
+    }
+}
---
+++ app/Http/Requests/Auth/RegisterRequest.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Requests\Auth;
+
+use App\Traits\FailValidationTrait;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    use FailValidationTrait;
+
+    /**
+     * Indicates whether validation should stop after the first rule failure.
+     *
+     * @var bool
+     */
+    protected $stopOnFirstFailure = true;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'confirmed', Password::min(8)],
+            'role_id' => ['required', 'exists:roles,id'],
+        ];
+    }
+
+    /**
+     * Handle a passed validation attempt.
+     */
+    protected function passedValidation(): void
+    {
+        $this->merge([
+            'credit' => $this->getCredit($this->role_id),
+        ]);
+    }
+
+    public function getCredit($roleId): NULL|int
+    {
+        $credit;
+        switch ($roleId) {
+            case 1:
+                $credit = 40;
+                break;
+            case 3:
+                $credit = 20;
+                break;
+            default:
+                $credit = null;
+                break;
+        }
+
+        return $credit;
+    }
+}
