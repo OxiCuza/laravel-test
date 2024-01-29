a46df624e52ac8f7bfb82094711081f9f4dbc0b2
oxicuza <oxicusa@gmail.com>
2024-01-29 16:04:32 +0700

    feat(room): create resource and collection

---
+++ app/Http/Resources/RoomResource.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class RoomResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'location' => $this->location,
+            'price' => $this->price,
+            'image' => $this->image,
+            'created_at' => $this->created_at,
+            'owner' => new UserResource($this->whenLoaded('owner')),
+        ];
+    }
+}
