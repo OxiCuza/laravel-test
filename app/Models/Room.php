62b6b526df838051413fcfaddcf6b213b336da28
oxicuza <oxicusa@gmail.com>
2024-01-30 08:47:28 +0700

    feat(model): add local query scope in room model

--- app/Models/Room.php
+++ app/Models/Room.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -37,5 +38,10 @@ class Room extends Model
     public function discussions(): HasMany
     {
         return $this->hasMany(RoomDiscussion::class);
+    }
+
+    public function scopeOwnedBy(Builder $query, $userId): void
+    {
+        $query->where('owner_id', $userId);
     }
 }
