56c72bdc047eb2c506507a13b31b98eb239da8c4
oxicuza <oxicusa@gmail.com>
2024-01-30 08:49:23 +0700

    ref(controller): refactor code in room controller

--- app/Http/Controllers/Api/RoomController.php
+++ app/Http/Controllers/Api/RoomController.php
@@ -3,10 +3,14 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Room\StoreRequest;
+use App\Http\Requests\Room\UpdateRequest;
 use App\Http\Resources\ErrorResource;
 use App\Http\Resources\RoomCollection;
 use App\Http\Resources\RoomResource;
+use App\Models\Role;
 use App\Models\Room;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,20 +18,26 @@ class RoomController extends Controller
 {
     public function index(Request $request): RoomCollection
     {
-        $room = Room::with('details')->paginate(5);
+        $user = $request->user();
+        $query = Room::with('details');
+
+        if ($user->role_id == Role::OWNER) {
+            $query->ownedBy($user->id);
+        }
+
+        $room = $query->paginate(10);
 
         return new RoomCollection($room);
     }
 
-    public function store(Request $request): JsonResponse
+    public function store(StoreRequest $request): JsonResponse
     {
-        $data = $request->validate([
-            'name' => 'required',
-            'price' => 'required',
-            'location' => 'required',
-            'owner_id' => 'required',
-            'details' => 'required|array|min:1',
-            'details.*.name' => 'required',
+        $data = $request->only([
+            'name',
+            'price',
+            'location',
+            'owner_id',
+            'details',
         ]);
 
         try {
@@ -50,30 +60,30 @@ class RoomController extends Controller
         try {
             $room = Room::with(['owner', 'details', 'discussions'])->find($id);
 
-            return response()->api(true, 'OK!', new RoomResource($room));
-        } catch (\Throwable $e) {
-            $res = [
-                'status' => false,
-                'message' => $e->getMessage(),
-            ];
+            $this->authorize('index', $room);
 
-            return response()->api(true, 'OK!', new ErrorResource($res, 500));
+            return response()->api(true, 'OK!', new RoomResource($room));
+        } catch (AuthorizationException $e) {
+            return response()->api(false, $e->getMessage(), null, 403);
+        } catch (\Throwable $e) {
+            return response()->api(false, $e->getMessage(), null, 500);
         }
     }
 
-    public function update(Request $request, $id): JsonResponse
+    public function update(UpdateRequest $request, $id): JsonResponse
     {
-        $data = $request->validate([
-            'name' => 'required',
-            'price' => 'required',
-            'location' => 'required',
-            'details' => 'nullable|array|min:1',
-            'details.*.id' => 'required|exists:room_details,id',
-            'details.*.name' => 'required',
+        $data = $request->only([
+            'name',
+            'price',
+            'location',
+            'details',
         ]);
 
         try {
             $room = Room::with('owner')->findOrFail($id);
+
+            $this->authorize('index', $room);
+
             $room->update($data);
 
             if ($data['details']) {
@@ -81,22 +91,27 @@ class RoomController extends Controller
             }
 
             return response()->api(true, 'OK!', new RoomResource($room));
+        } catch (AuthorizationException $e) {
+            return response()->api(false, $e->getMessage(), null, 403);
         } catch (\Throwable $e) {
-            $res = [
-                'status' => false,
-                'message' => $e->getMessage(),
-            ];
-
-            return response()->api(true, 'OK!', new ErrorResource($res, 500));
+            return response()->api(false, $e->getMessage(), null, 500);
         }
     }
 
     public function destroy(Request $request, $id): JsonResponse
     {
-        if (Room::destroy($id)) {
-            return response()->api(true, 'OK!');
-        }
+        try {
+            $room = Room::findOrFail($id);
 
-        return response()->api(false, 'Failed!');
+            $this->authorize('index', $room);
+
+            $room->delete();
+
+            return response()->api(true, 'OK!');
+        } catch (AuthorizationException $e) {
+            return response()->api(false, $e->getMessage(), null, 403);
+        } catch (\Throwable $e) {
+            return response()->api(false, $e->getMessage(), null, 500);
+        }
     }
 }
